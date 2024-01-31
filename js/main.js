5c83fe7ca6b1090c46d6080e667701cf263b878e
Hsing Chen <twrandy24@gmail.com>
2024-02-01 01:43:49 +0800

    刪除不必要的資料夾，調整資料位置

--- js/main.js
+++ js/main.js
@@ -4,20 +4,95 @@ const answerButton = document.getElementById('answer');
 const showBoxElement = document.getElementById('showBox');
 const questionListElement = document.getElementById('questionList');
 const playedListElement = document.getElementById('playedList');
-let questionData;
-let answerData;
-let showQuestionList;
-
-function getData() {
-	Promise.all([
-		fetch('./../data/question.json').then(response => response.json()),
-		fetch('./../data/answer.json').then(response => response.json())
-	]).then(dataArray => {
-		questionData = dataArray[0];
-		answerData = dataArray[1];
-		document.body.classList.add('dataLoaded');
-	});
-}
+let questionData = [
+	{
+		"id": 0,
+		"name": "一分鐘伏地挺身",
+		"keyword": [2, 3]
+	},
+	{
+		"id": 1,
+		"name": "年紀",
+		"keyword": [4, 5]
+	},
+	{
+		"id": 2,
+		"name": "年資",
+		"keyword": [0, 1]
+	},
+	{
+		"id": 3,
+		"name": "身高",
+		"keyword": [8, 9]
+	},
+	{
+		"id": 4,
+		"name": "大腿",
+		"keyword": [0, 1, 6, 7]
+	},
+	{
+		"id": 5,
+		"name": "臂展",
+		"keyword": [0, 1]
+	},
+	{
+		"id": 6,
+		"name": "頭髮",
+		"keyword": [0, 1]
+	},
+	{
+		"id": 7,
+		"name": "眼睛",
+		"keyword": [0, 1, 4, 5]
+	},
+	{
+		"id": 8,
+		"name": "中指",
+		"keyword": [0, 1, 6, 7]
+	},
+	{
+		"id": 9,
+		"name": "兄弟姐妹",
+		"keyword": [2, 3]
+	},
+	{
+		"id": 10,
+		"name": "未讀 LINE 訊息",
+		"keyword": [2, 3]
+	},
+	{
+		"id": 11,
+		"name": "手機年齡(以上市日為主)",
+		"keyword": [4, 5]
+	},
+	{
+		"id": 12,
+		"name": "男/女朋友",
+		"keyword": [2, 3]
+	},
+	{
+		"id": 13,
+		"name": "大家來找碴",
+		"keyword": [2, 3]
+	},
+	{
+		"id": 14,
+		"name": "寫字",
+		"keyword": [2, 3]
+	}
+];
+const answerData = [
+	"長",
+	"短",
+	"多",
+	"少",
+	"大",
+	"小",
+	"粗",
+	"細",
+	"高",
+	"矮"
+];
 
 function getRandom(length) {
 	return Math.floor(Math.random() * length);
@@ -99,7 +174,7 @@ function record() {
 }
 
 window.addEventListener('load', ()=>{
-	getData();
+	document.body.classList.add('dataLoaded');
 });
 
 startButton.addEventListener('click', function(e){
